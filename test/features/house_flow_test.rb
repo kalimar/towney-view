2950413e253e4f6a0552e921613ac802155115fe
Kalimar Maia <kalimar.maia@gmail.com>
2015-05-19 21:45:26 -0400

    Add some content to welcome page

--- test/features/house_flow_test.rb
+++ test/features/house_flow_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 class HouseFlowTest < Capybara::Rails::TestCase
   test "home page" do
     visit root_path
-    page.has_content "neighbors"
-
+    page.must_have_content "Neighbors"
+    page.must_have_content "Service Recommendations"
+    page.must_have_content "Photos"
+    page.must_have_content "My House"
   end
 end
