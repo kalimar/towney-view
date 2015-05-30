558c351ce52af12a37726565938f90debfe61d81
Kalimar Maia <kmaia@customink.com>
2015-05-29 21:42:27 -0300

    Add parameters to Home model and sanitize
    Added several columns to Home Model and devise sanitizer method to allow account to have several more fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << [:primary_first_name, :primary_last_name, :street_address, :primary_phone, :primary_birth_day, :primary_birth_month]
+    devise_parameter_sanitizer.for(:sign_up) << [:primary_first_name, :primary_last_name, :street_address, :primary_phone, :primary_birth_day, :primary_birth_month]
+  end
 end
---
+++ test/helpers/homes_helper_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class HomesHelperTest < ActionView::TestCase
+
+  def test_sanity
+    flunk "Need real tests"
+  end
+
+end
