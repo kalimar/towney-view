ecba0e4d59e8e666b3d515014a056595a36a0118
Kalimar Maia <kmaia@customink.com>
2015-05-30 22:28:47 -0300

    Add app roadmap

--- test/features/house_flow_test.rb
+++ test/features/house_flow_test.rb
@@ -7,5 +7,6 @@ class HouseFlowTest < Capybara::Rails::TestCase
     page.must_have_content "Service Recommendations"
     page.must_have_content "Photos"
     page.must_have_content "My House"
+    page.must_have_content "Travel"
   end
 end
