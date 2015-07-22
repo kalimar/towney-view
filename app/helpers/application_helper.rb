77217600c80a48a6e3c893389501e3038e6b3446
Kalimar Maia <kmaia@customink.com>
2015-07-22 19:01:38 -0400

    Clean up css for home page and prepare for basic release

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def avatar size
+    url = [
+      "http://placebear.com/#{size.to_s}/#{size.to_s}",
+      "http://lorempixel.com/#{size.to_s}/#{size.to_s}/animals",
+       "http://lorempixel.com/#{size.to_s}/#{size.to_s}/cats"
+    ].sample
+    image_tag url, alt:"avatar", class:"image-object"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :homes
-  resources :homes
   root 'welcome#index'
+  resources :homes
 end
